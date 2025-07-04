aa011864bb94cb773895f7c68fc8f86ddee99778
YammD123 <siamqueen52@gmail.com>
2025-07-04 17:19:17 +0700

    feat(comments): add model comments

--- app/validators/comment.ts
+++ app/validators/comment.ts
@@ -1,1 +1,8 @@
 import vine from '@vinejs/vine'
+
+export const commentRootValidator = vine.compile(
+    vine.object({
+        postId: vine.string(),
+        comment: vine.string()
+    })
+)
--- start/routes.ts
+++ start/routes.ts
@@ -29,11 +29,10 @@ router.get('/', async () => {
 
 //api route
 router.group(() => {
-
   // auth route
   router.group(() => {
     router.post('/register', [AuthController, 'register']).use(middleware.guest()),
-    router.post('/login', [AuthController, 'login']),
+    router.post('/login', [AuthController, 'login']).use(middleware.guest()),
     router.post('/logout', [AuthController, 'logout']).use(middleware.auth())
     router.get('/session', [AuthController, 'getSession']).use(middleware.auth())
   }).prefix('auth')
