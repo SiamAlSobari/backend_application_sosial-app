0f565197549de7096d552f40e2cd9eedcea2361d
YammD123 <siamqueen52@gmail.com>
2025-07-04 15:15:41 +0700

    feat(comments) add validator comments file

--- app/controllers/comments_controller.ts
+++ app/controllers/comments_controller.ts
@@ -1,5 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class CommentsController {
-    public async createRootComment({request,response,auth}: HttpContext) {}
+    public async createRootComment({request,response,auth}: HttpContext) {
+        const payload = await request.validateUsing
+    }
 }
