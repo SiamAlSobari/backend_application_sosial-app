52a25eb152d6c0a089976b4878c48c8d736559f7
YammD123 <siamqueen52@gmail.com>
2025-07-09 15:49:35 +0700

    feat(notifications): enhance getSomeNotifications method to preload sender and profile data

--- app/controllers/notifications_controller.ts
+++ app/controllers/notifications_controller.ts
@@ -2,11 +2,18 @@ import Notification from '#models/notification'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class NotificationsController {
-    public async getSomeNotifications({response,request,auth}: HttpContext) {
-        const notification = await Notification.query().where('receiver_id',auth.user!.id).where('is_read',false).orderBy('created_at','desc').limit(5)
-        response.status(200).json({
-            message: 'Notifications fetched successfully',
-            data: notification
-        })
-    }
+  public async getSomeNotifications({ response, auth }: HttpContext) {
+    const notification = await Notification.query()
+      .where('receiver_id', auth.user!.id)
+      .where('is_read', false)
+      .orderBy('created_at', 'desc')
+      .limit(5)
+      .preload('sender', (query) => {
+        query.preload('profile')
+      })
+    response.status(200).json({
+      message: 'Notifications fetched successfully',
+      data: notification,
+    })
+  }
 }
