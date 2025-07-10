d4790efe24835d3d773bc2994d10e3b5c884d6b2
YammD123 <siamqueen52@gmail.com>
2025-07-10 18:01:02 +0700

    feat(users): implement getAllUsers method to fetch users excluding the authenticated user

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,4 +1,14 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
+    public async getAllUsers({response,auth}: HttpContext) {
+        const user = await User.query().whereNot('id',auth.user!.id).preload('profile').whereDoesntHave('followers', (query) => {
+            query.where('follower_id', auth.user!.id)
+        }).preload('profile')
+        response.status(200).json({
+            message: 'Users fetched successfully',
+            data: user
+        })
+    }
 }
